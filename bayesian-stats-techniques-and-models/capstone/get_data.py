acc2723202f741755a9aee0866539d9e1b0c421a
Ben Hammel <bdhammel@gmail.com>
2019-12-23 09:49:34 -0800

    Include Capstone report

--- bayesian-stats-techniques-and-models/capstone/get_data.py
+++ bayesian-stats-techniques-and-models/capstone/get_data.py
@@ -3,8 +3,8 @@ import requests
 import pandas as pd
 import numpy as np
 from datetime import datetime, timedelta
-import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
+import matplotlib.pyplot as plt
 
 
 # https://maps.waterdata.usgs.gov/mapper/
@@ -12,7 +12,7 @@ from scipy.interpolate import interp1d
 # https://waterdata.usgs.gov/nwis/current/?type=precip&group_key=state_cd
 # https://waterservices.usgs.gov/rest/DV-Test-Tool.html
 # http://cdec.water.ca.gov/jspplot/jspPlotServlet.jsp?sensor_no=11108&end=12%2F14%2F2019+22%3A25&geom=huge&interval=600&cookies=cdec01
-# http://cdec.water.ca.gov/dynamicapp/staMeta?station_id=TK2 
+# http://cdec.water.ca.gov/dynamicapp/staMeta?station_id=TK2
 # http://cdec.water.ca.gov/dynamicapp/selectSnow
 # https://www.nohrsc.noaa.gov/interactive/html/graph.html?station=TADC1&w=600&h=400&o=a&uc=0&by=2015&bm=12&bd=10&bh=6&ey=2019&em=12&ed=12&eh=5&data=2&units=0&region=us
 # http://cdec.water.ca.gov/dynamicapp/wsSensorData
@@ -33,13 +33,11 @@ from scipy.interpolate import interp1d
 # Donner: 10338400
 # Boca: 10344490
 
+START = '2017-01-01'
+END = '2019-12-01'
 
-RAIN_URL = "https://waterservices.usgs.gov/nwis/dv/?format=json&sites=391930120165301&period=P100W&siteStatus=all"
-RIVER_URL = "https://waterservices.usgs.gov/nwis/dv/?format=json&sites={site_id}&period=P100W&parameterCd=00060"
-LAKE_URL = "https://waterservices.usgs.gov/nwis/dv/?format=json&sites={site_id}&period=P100W&parameterCd=00065"
-SNOW_URL = "https://cdec.water.ca.gov/dynamicapp/req/JSONDataServlet?Stations=TK2&SensorNums=82&end=2019-12-15&span=100weeks"
-TEMP_URL = "https://www.ncei.noaa.gov/access/services/data/v1?dataset=daily-summaries&stations=USS0020K13S&startDate=2016-01-01&endDate=2019-12-10&format=JSON"
-
+RIVER_URL = "https://waterservices.usgs.gov/nwis/dv/?format=json&sites={site_id}&startDT={START}&endDT={END}&parameterCd=00060"
+WEATHER_URL = f'https://www.ncei.noaa.gov/access/services/data/v1?dataset=daily-summaries&stations=USS0020K13S&startDate={START}&endDate={END}&format=JSON&units=metric'
 TRUCKEE = 10346000
 TAHOE = 10337500
 DONNER = 10338500
@@ -60,50 +58,27 @@ def get_river_data(url):
     data_dict = {'time': data[0], 'value': data[1]}
 
     df = pd.DataFrame(data=data_dict)
-    return df
+
+    def _clean_river_data(df):
+        drop_idx = df.value[df.value == 0].index
+        df.drop(drop_idx, inplace=True)
+        df.value = df.value.apply(lambda x: .02832*x)  # ft^3 to m^3
+        return df
+
+    return _clean_river_data(df)
 
 
-def _get_from_url(data_name):
-    url = {
-        'river': RIVER_URL,
-        'rain': RAIN_URL,
-        'lake': LAKE_URL,
-        'snow': SNOW_URL,
-    }[data_name]
+def get_climate_data():
+    res = json.loads(requests.get(WEATHER_URL).content)
+    df = pd.DataFrame(res)
+    df = df.drop(columns=['STATION', 'TMIN', 'TMAX', 'TOBS'])
 
-    res = json.loads(requests.get(url).content)
-
-    if data_name == "snow":
-        datatime_str = "%Y-%m-%d %H:%M"
-        data = list(zip(*[
-            [datetime.strptime(datum['obsDate'], datatime_str), float(datum['value'])]
-            for datum in res
-        ]))
-    else:
-        datatime_str = "%Y-%m-%dT%H:%M:%S.%f"
-        d = res['value']['timeSeries'][0]['values'][0]['value']
-
-        data = list(zip(*[
-            [datetime.strptime(datum['dateTime'], datatime_str), float(datum['value'])]
-            for datum in d
-        ]))
-
-    data_dict = {'time': data[0], 'value': data[1]}
-
-    df = pd.DataFrame(data=data_dict)
-    # df.set_index('time')
-    df.to_pickle(f'data/{data_name}.pkl')
-    return df
-
-
-def _clean_snow_data(df):
-    df.value = df.value.apply(lambda x: max(0, x))
-    return df
-
-
-def _clean_river_data(df):
-    drop_idx = df.value[df.value == 0].index
-    df.drop(drop_idx, inplace=True)
+    cols = df.columns.drop('DATE')
+    df[cols] = df[cols].apply(pd.to_numeric, errors='coerce')
+    datatime_str = "%Y-%m-%d"
+    df.DATE = df.DATE.apply(
+        lambda x: datetime.strptime(x, datatime_str)
+    )
     return df
 
 
@@ -111,59 +86,49 @@ def _get_from_file(data_name):
     return pd.read_pickle(f'data/{data_name}.pkl')
 
 
-def get_data(data_name):
-    # return _get_from_url(data_name)
-    return _get_from_file(data_name)
-
-
-def get_climate_data():
-    url = 'https://www.ncei.noaa.gov/access/services/data/v1?dataset=daily-summaries&stations=USS0020K13S&startDate=2017-01-01&endDate=2019-12-10&format=JSON&units=imperial'
-    res = json.loads(requests.get(url).content)
-    df = pd.DataFrame(res)
-    cols = df.columns.drop('DATE', 'STATION')
-    df[cols] = df[cols].apply(pd.to_numeric, errors='coerce')
-    return df
-
-
-def get_value_fn(df):
+def get_river_fn(df):
     return interp1d(df.time.values.astype(float), df.value.values, bounds_error=False, kind='linear')
 
 
-def moving_average(a, n=5):
-    ret = np.cumsum(a, dtype=float)
-    ret[n:] = ret[n:] - ret[:-n]
-    ret[n - 1:] /= n
-    return ret
+def get_weather_fn(df):
+    def _get_fn(time, values):
+        return interp1d(time.astype(float), values, bounds_error=False, kind='linear')
+
+    cols = df.columns.drop('DATE')
+    funs = {
+        col: _get_fn(df.DATE.values, df[col].values) for col in cols
+    }
+    return funs
 
 
-truckee_data = get_river_data(RIVER_URL.format(site_id=TRUCKEE))
-truckee_data = _clean_river_data(truckee_data)
-tahoe_data = get_river_data(RIVER_URL.format(site_id=TAHOE))
-donner_data = get_river_data(RIVER_URL.format(site_id=DONNER))
-boca_data = get_river_data(RIVER_URL.format(site_id=BOCA))
-# tahoe_data = _clean_river_data(truckee_data)
+truckee_data = get_river_data(RIVER_URL.format(site_id=TRUCKEE, START=START, END=END))
+tahoe_data = get_river_data(RIVER_URL.format(site_id=TAHOE, START=START, END=END))
+donner_data = get_river_data(RIVER_URL.format(site_id=DONNER, START=START, END=END))
+boca_data = get_river_data(RIVER_URL.format(site_id=BOCA, START=START, END=END))
 
-truckee_data.plot('time', 'value')
-tahoe_data.plot('time', 'value')
-donner_data.plot('time', 'value')
-boca_data.plot('time', 'value')
+weather_data = get_climate_data()
+weather_fns = get_weather_fn(weather_data)
 
-rain_data = get_data('rain')
-snow_data = _clean_snow_data(get_data('snow'))
+truckee_fn = get_river_fn(truckee_data)
+tahoe_fn = get_river_fn(tahoe_data)
+boca_fn = get_river_fn(boca_data)
+donner_fn = get_river_fn(donner_data)
 
-truckee_fn = get_value_fn(truckee_data)
-rain_fn = get_value_fn(rain_data)
-lake_fn = get_value_fn(tahoe_data)
-snow_fn = get_value_fn(snow_data)
 _time = truckee_data.time.values
+time = np.arange(_time[0], _time[-1], timedelta(days=1), dtype=_time.dtype)
+float_time = time.astype(int)
 
-time = np.arange(_time[0], _time[-1], timedelta(days=1), dtype=_time.dtype).astype(float)
+river_data = pd.DataFrame({
+    'time': time,
+    'truckee': truckee_fn(float_time),
+    'tahoe': tahoe_fn(float_time),
+    'donner': donner_fn(float_time),
+    'boca': boca_fn(float_time),
+    'rain': weather_fns['PRCP'](float_time),
+    'snow': weather_fns['WESD'](float_time),
+    'temp': weather_fns['TAVG'](float_time),
+})
 
+plt.plot(river_data.truckee, river_data.tahoe + river_data.donner + river_data.boca, 'o')
 
-fig, axs = plt.subplots(4, 1, sharex=True)
-
-axs[0].plot(time, truckee_fn(time))
-avg_rain = moving_average(rain_fn(time))
-axs[1].plot(time, avg_rain)
-axs[2].plot(time, lake_fn(time))
-axs[3].plot(time, snow_fn(time))
+river_data.to_pickle('./data/clean_data.pkl')
